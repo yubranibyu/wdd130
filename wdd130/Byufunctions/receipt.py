fe6d7e01b5ad1b19ec915f7de2900eac56c0fb32
yubrani <yag24001@byui.edu>
2024-10-21 00:15:04 -0300

    fift commit again

--- wdd130/Byufunctions/receipt.py
+++ wdd130/Byufunctions/receipt.py
@@ -14,70 +14,39 @@ def main():
     products_dict = read_dictionary("products.csv", KEY_COLUMN_INDEX)
     request_dict = read_dictionary("request.csv", KEY_COLUMN_INDEX)
     print(f"Inkom Emporium")
-    #print("All products")
-    #print(products_dict)
     total_items = 0
     subtotal = 0
     tax = 0.06
     total = 0
     tax_amount = 0
 
-   
-   
-
-
-
-    for i in products_dict and request_dict:
-       
-
-        
-        key = products_dict[i][0]
-        product = products_dict[i][1]
-        price = products_dict[i][2]
-        key_request = request_dict[i][0]
-        quantity_request = request_dict[i][1]
-        
-        total_items += float(quantity_request)
-        subtotal += float(price)
-        print(f"{product} {quantity_request} {price} ")
-    print(f"Total of items: {total_items}")
-    print(f"The subtotal is: {subtotal:.3f}$")
-    total = subtotal + (subtotal * tax)
-    tax_amount = subtotal * tax
-    print(f"Sales tax: {tax_amount:.2f}")
-
-    print(f"Total: {total:.3f}$")
-    print("Thank you for shopping at: Inkom Emporeum")
-    print(f"{current_date_and_time:%A %I:%M %p}")
-    
     try:
-            key = products_dict[i][0]
+
+        products_dict = read_dictionary("products.csv", KEY_COLUMN_INDEX)
+        request_dict = read_dictionary("request.csv", KEY_COLUMN_INDEX)
+
+        for i in products_dict and request_dict:
+        
             product = products_dict[i][1]
             price = products_dict[i][2]
-            key_request = request_dict[i][0]
             quantity_request = request_dict[i][1]
-            
             total_items += float(quantity_request)
             subtotal += float(price)
+            
             print(f"{product} {quantity_request} {price} ")
-            print(f"Total of items: {total_items}")
-            print(f"The subtotal is: {subtotal:.3f}$")
-            total = subtotal + (subtotal * tax)
-            tax_amount = subtotal * tax
-            print(f"Sales tax: {tax_amount:.2f}")
+        print(f"Total of items: {total_items}")
+        print(f"The subtotal is: {subtotal:.3f}$")
+        total = subtotal + (subtotal * tax)
+        tax_amount = subtotal * tax
+        print(f"Sales tax: {tax_amount:.2f}")
 
-            print(f"Total: {total:.3f}$")
-            print("Thank you for shopping at: Inkom Emporeum")
-            print(f"{current_date_and_time:%A %I:%M %p}")
+        print(f"Total: {total:.3f}$")
+        print("Thank you for shopping at: Inkom Emporeum")
+        print(f"{current_date_and_time:%A %I:%M %p}")
+    
     except KeyError as keyerr:
-         print(keyerr)
-            
-            
-    
-  
-
-    
-
+        print("Error: missing file")
+        print("[Errno 2] No such file or directory: 'products.csv'")
 
 
 def read_dictionary(filename, key_column_index):
@@ -94,18 +63,7 @@ def read_dictionary(filename, key_column_index):
             index = row_list[key_column_index]
             dictionary[index] = row_list
 
-    """Read the contents of a CSV file into a compound
-  dictionary and return the dictionary.
-  Parameters
 
-      
-  
-      filename: the name of the CSV file to read.
-      key_column_index: the index of the column
-          to use as the keys in the dictionary.
-  Return: a compound dictionary that contains
-      the contents of the CSV file.
-  """
     return dictionary
 if __name__ == "__main__":
     main()
