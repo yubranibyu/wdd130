25e3644b050dc03e8582fad5035e0e7a58ba324c
yubrani <yag24001@byui.edu>
2024-10-23 01:52:41 -0300

    fiftcommit again againx2

--- wdd130/Byufunctions/test.py
+++
@@ -1,9 +0,0 @@
-def main():
-  try:
-    with open("products.vcs", "rt") as products_file:
-        for row in products_file:
-            print(row)
-  except FileNotFoundError as not_found_err:
-    print(not_found_err)
-if __name__ == "__main__":
-  main()
